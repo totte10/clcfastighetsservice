863f403e9734f52337b7ace0d88aeb880a12e962
totte10 <totte@celecinvest.com>
2026-03-16 08:17:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import AdminDashboard from "@/pages/AdminDashboard";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
